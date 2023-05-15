eb04efd7d7137833137494f798b75588900fe040
Raghav <kumarraghav71950@gmail.com>
2023-05-15 12:08:47 +0530

    adding middlewares and routes

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -14,7 +14,25 @@ Possible Cases:
 function grantAccessTo(roles) {
   return function (req, res, next) {
     //Write your code here;
-    try{
+    const token = req.headers.authorization;
+    if (!token) {
+      return res
+        .status(401)
+        .json({
+          message: "Authentication failed: Missing token.",
+          status: "Error",
+        });
+    }
+
+    try {
+      const decodedToken = jwt.verify(token, JWT_SECRET);
+      if (!roles.includes(decodedToken.role)) {
+        return res
+          .status(403)
+          .json({ message: "Access Denied", status: "Error" });
+      }
+      next();
+      
     } catch (err) {
       return res.status(401).json({ message: 'Authentication failed: Invalid token.', status: "Error" });
     }
